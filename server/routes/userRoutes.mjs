1d6cf308c5d5c4ba55e72e88fd667b45778bc5b1
Daria Steblovska <darias1896.96@gmail.com>
2025-11-25 21:10:22 +0000

    users routes updated

--- server/routes/userRoutes.mjs
+++ server/routes/userRoutes.mjs
@@ -6,6 +6,19 @@ const router = Router()
 router
  .route('/')
  .get(userController.getAllUsers)
-//  .post(userController.createUser)
+ .post(userController.createUser)
+
+router.route("/:id").get(userController.getOneUser);
+
+router
+  .route("/me")
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser)
+
+router
+  .route("/auth")
+  .post(userController.loginUser)
+
+router.route("/logout").post(userController.logoutUser)
 
  export default router;
