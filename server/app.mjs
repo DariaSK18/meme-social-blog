da7d4afbb8cc1d7c8274cb30713390c4cee34c3f
Daria Steblovska <darias1896.96@gmail.com>
2025-11-25 04:18:55 +0000

    back: app route tested

--- server/app.mjs
+++ server/app.mjs
@@ -1,14 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import errorHandler from "./middleware/errorHandler.mjs";
+import AppError from "./utils/AppError.mjs";
 
 dotenv.config()
 
 const app = express()
 
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     console.log(req.originalUrl);
+// })
+
 
 app.use((req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
-});
+    console.log(req.originalUrl, req.method)
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
+})
 
 app.use(errorHandler);
 
