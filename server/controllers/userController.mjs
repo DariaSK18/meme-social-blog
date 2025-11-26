dc49ad4f143beb7f1118e5faba84c9194c547d65
Daria Steblovska <darias1896.96@gmail.com>
2025-11-26 11:10:35 +0000

    back: delete user profile controller created

--- server/controllers/userController.mjs
+++ server/controllers/userController.mjs
@@ -2,6 +2,9 @@ import { catchAsync } from "../utils/catchAsync.mjs";
 // import AppError from "../utils/AppError.mjs";
 // import User from "../models/user.mjs";
 import User from "../models/user.mjs";
+import AppError from "../utils/AppError.mjs";
+import RefreshToken from "../models/refreshToken.mjs";
+import Meme from "../models/meme.mjs";
 
 // const users = ["Daria", "Burcu", "Anna", "Steven"];
 
@@ -27,3 +30,18 @@ export const getOneUser = catchAsync(async (req, res, next) => {
 // --- update user by id ---
 
 // --- delete user profile ---
+
+export const deleteUser = catchAsync(async (req, res, next) => {
+  const {
+    body: { id },
+  } = req;
+  // delete all his post if user delete profile (implement when the posts done)
+  // or change name 'Meme' to 'Post' ???
+  await Meme.destroy({ where: { user_id: id } });
+
+  const deleted = await User.destroy({ where: { id } });
+  if (!deleted) return next(new AppError("User not found", 404));
+  await RefreshToken.destroy({ where: { user_id: id } });
+  res.clearCookie("refreshToken");
+  res.status(200).json({ msg: "Account deleted successfully" });
+});
