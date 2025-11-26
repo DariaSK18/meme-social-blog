383cd0ac9c702209f202a7613326a5c4d3dba74d
Daria Steblovska <darias1896.96@gmail.com>
2025-11-26 02:57:07 +0000

    back: testing code deleted

--- server/app.mjs
+++ server/app.mjs
@@ -6,78 +6,17 @@ import routes from "./routes/index.mjs";
 
 // --- test
 
-
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     console.log(req.originalUrl);
-// })
-
-// ---- testing
-const posts = [
-  {
-    username: "Daria",
-    title: "Post 1",
-  },
-  {
-    username: "Burcu",
-    title: "Post 2",
-  },
-];
-
-app.get("/posts", authenticateToken, (req, res) => {
-  res.json(posts.filter((post) => post.username === req.user.name));
+// --- main page test server ---
+app.get("/", (req, res) => {
+  console.log(req.originalUrl);
 });
-
-let refreshTokens = [];
-
-app.post("/token", (req, res) => {
-  const refreshToken = req.body.token;
-  if (!refreshToken) return res.sendStatus(401);
-  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-
-    console.log("decoded refresh:", user);
-    const accessToken = jwt.sign(
-      { name: user.name },
-      process.env.ACCESS_TOKEN_SECRET,
-      {
-        expiresIn: "30s",
-      }
-    );
-    res.json({ accessToken });
-  });
-});
-
-app.delete('/logout', (req, res) =>{ 
-  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-  res.sendStatus(204)
-})
-
-app.post("/login", (req, res) => {
-  const {
-    body: { username },
-  } = req;
-
-  const user = { name: username };
-
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "30s",
-  });
-
-  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
-  refreshTokens.push(refreshToken);
-  res.json({ accessToken, refreshToken });
-});
-
-
-
-// --------------------
+// -----------------------------
 
 app.use("/api", routes);
 
