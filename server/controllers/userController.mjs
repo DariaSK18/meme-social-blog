40a0f6ff2e33a4ed0fc80de8cdd67295b155fb44
Daria Steblovska <darias1896.96@gmail.com>
2025-11-26 11:03:11 +0000

    back: find single user by id implemented

--- server/controllers/userController.mjs
+++ server/controllers/userController.mjs
@@ -3,21 +3,27 @@ import { catchAsync } from "../utils/catchAsync.mjs";
 // import User from "../models/user.mjs";
 import User from "../models/user.mjs";
 
-
-const users = ["Daria", "Burcu", "Anna", "Steven"];
+// const users = ["Daria", "Burcu", "Anna", "Steven"];
 
 // --- get all users (for checks or admin user only) ---
 export const getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.findAll()
+  const users = await User.findAll();
   console.log(users);
-  
+
   res.status(200).json(users);
 });
 
 // --- get single user by id ---
 
+export const getOneUser = catchAsync(async (req, res, next) => {
+  const {
+    body: { id },
+  } = req;
+  const user = await User.findOne({ where: id });
+  if (!user) return next(new AppError("User not found", 404));
+  res.status(200).json(user);
+});
+
 // --- update user by id ---
 
 // --- delete user profile ---
-
-
