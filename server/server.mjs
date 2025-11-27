06f136bef67d31d4b9a79c78d5105780baf46ca9
Burcu Gunes <burcudagli.10@gmail.com>
2025-11-27 14:57:57 +0000

    cloudinary set up for images

--- server/server.mjs
+++ server/server.mjs
@@ -1,15 +1,19 @@
 import app from "./app.mjs";
 import sequelize from "./config/connection.mjs";
 import "./models/index.mjs";
+import { fetchImage,uploadImage } from "./utils/helpers/imageManager.mjs";
+import dotevn from "dotenv";
+dotevn.config();
+
+// uploadImage("./img/wireframe-blog.png")
+// const image=fetchImage("")
+// console.log(image);
 
 
 const PORT = process.env.PORT || 3000;
 const HOST = "0.0.0.0";
 
-
-
 const rebuild = process.argv[2] === "--rebuild";
-
 
 sequelize.sync({ force: rebuild }).then(() => {
   app.listen(PORT, () => {
@@ -18,3 +22,4 @@ sequelize.sync({ force: rebuild }).then(() => {
   });
 
 });
+
---
+++ server/utils/helpers/CloudinaryControls.mjs
@@ -0,0 +1,6 @@
+import cloudinary from "../../config/cloudinary.mjs";
+
+// Test Cloudinary connection
+cloudinary.api.ping()
+  .then(res => console.log("Cloudinary OK:", res))
+  .catch(err => console.log("Cloudinary ERROR:", err));
