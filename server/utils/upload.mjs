ba44e5890483f83978dca7b53eb4ede71094eaa1
Daria Steblovska <darias1896.96@gmail.com>
2025-11-27 13:52:12 +0000

    back: cloudinary config file and upload created in utils

---
+++ server/utils/upload.mjs
@@ -0,0 +1,16 @@
+import { v2 as cloudinary } from "./cloudinaryConfig.mjs";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "memes",
+    allowed_formats: ["jpg","jpeg","png","gif"],
+    transformation: [{ width: 800, crop: "limit" }],
+  },
+});
+
+const parser = multer({ storage });
+
+export default parser;
