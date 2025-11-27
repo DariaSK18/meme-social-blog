e77ead62e57cb3ddbb2b3d30535ce86e34bf70ad
Daria Steblovska <darias1896.96@gmail.com>
2025-11-27 11:48:48 +0000

    back: followers and following list controller created

--- server/controllers/followController.mjs
+++ server/controllers/followController.mjs
@@ -5,36 +5,75 @@ import Follow from "../models/follow.mjs";
 
 // --- toggle follow or unfollow user ---
 export const toggleFollow = catchAsync(async (req, res, next) => {
-  const targetUserId = req.params.id
+  const targetUserId = req.params.id;
   const currentUserId = req.user.id;
 
-  if(currentUserId.toString() === targetUserId.toString()) return next(new AppError("You can not follow yourself", 400))
+  if (currentUserId.toString() === targetUserId.toString())
+    return next(new AppError("You can not follow yourself", 400));
 
-  const existing = await Follow.findOne({where: {follower_id: currentUserId, following_id: targetUserId}})
+  const existing = await Follow.findOne({
+    where: { follower_id: currentUserId, following_id: targetUserId },
+  });
 
-  let following
+  let following;
 
-  if(existing) {
-    await existing.destroy()
-    following = false
-  }else{
+  if (existing) {
+    await existing.destroy();
+    following = false;
+  } else {
     await Follow.create({
-        follower_id: currentUserId,
-        following_id: targetUserId
-    })
-    following = true
+      follower_id: currentUserId,
+      following_id: targetUserId,
+    });
+    following = true;
   }
 
   const followersCount = await Follow.count({
-    where: {following_id: targetUserId}
-  })
+    where: { following_id: targetUserId },
+  });
   const followingCount = await Follow.count({
-    where: {follower_id: currentUserId}
-  })
+    where: { follower_id: currentUserId },
+  });
 
   res.status(200).json({
     following,
     followersCount,
-    followingCount
+    followingCount,
   });
+});
+
+// --- get followers list ---
+
+export const getFollowers = catchAsync(async (req, res, next) => {
+  const {
+    params: { id },
+  } = req;
+
+  const followers = await Follow.findAll({
+    where: { following_id: id },
+    include: {
+      model: User,
+      as: "follower",
+      attributes: ["id", "username", "email"],
+    },
+  });
+  res.json(followers);
+});
+
+// --- get following list ---
+
+export const getFollowing = catchAsync(async (req, res, next) => {
+  const {
+    params: { id },
+  } = req;
+
+  const following = await Follow.findAll({
+    where: { follower_id: id },
+    include: {
+      model: User,
+      as: "following",
+      attributes: ["id", "username", "email"],
+    },
+  });
+  res.json(following);
 });
