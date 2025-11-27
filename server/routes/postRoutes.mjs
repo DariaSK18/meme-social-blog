41a932d38faf5b9d38a205f4b4e836c58712412b
Daria Steblovska <darias1896.96@gmail.com>
2025-11-27 06:44:57 +0000

    back: post routes created and auth middleware added to routes

--- server/routes/postRoutes.mjs
+++ server/routes/postRoutes.mjs
@@ -1,10 +1,23 @@
 import { Router } from 'express'
 import * as postController from '../controllers/postController.mjs'
+import authToken from "../middleware/authToken.mjs";
+import { isAuthor } from "../middleware/isUser.mjs";
 
 const router = Router()
 
 router
   .route("/")
   .get(postController.getAllBlogs)
+  .post(authToken, postController.createPost);
+
+router
+  .route("/:id")
+  .get(postController.getOnePost)
+  .patch(authToken, isAuthor, postController.updatePost)
+  .delete(authToken, isAuthor, postController.deletePost);
+
+router
+  .route("/:id/like")
+  .patch(authToken, postController.toggleLike)
 
 export default router
