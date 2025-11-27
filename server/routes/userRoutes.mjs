2048b2d853e269c1bfdfe161e21f203c0d150f88
Daria Steblovska <darias1896.96@gmail.com>
2025-11-27 14:18:09 +0000

    back: routes updated with field validation middlewares

--- server/routes/userRoutes.mjs
+++ server/routes/userRoutes.mjs
@@ -2,23 +2,29 @@ import { Router } from "express";
 import * as userController from "../controllers/userController.mjs";
 import * as authController from "../controllers/authController.mjs";
 import authToken from "../middleware/authToken.mjs";
-// import { isUser, isAuthor } from "../middleware/isUser.mjs";
+import { validate } from "../middleware/validate.mjs";
+import {
+  userPatch,
+  signupValidation,
+  userLogin,
+} from "../utils/helpers/validation.mjs";
+import { checkSchema } from "express-validator";
 
 const router = Router();
 
 router
   .route("/")
   .get(userController.getAllUsers) // ok
-  .post(authController.createUser); // ok
+  .post(checkSchema(signupValidation), validate, authController.createUser); // ok
 
 router.route("/:id").get(userController.getOneUser); // ok
 
 router
   .route("/me")
-  .patch(authToken, userController.updateUser) // ok
+  .patch(authToken, checkSchema(userPatch), validate, userController.updateUser) // ok
   .delete(authToken, userController.deleteUser); // ok
 
-router.route("/login").post(authController.loginUser); // ok
+router.route("/login").post(checkSchema(userLogin), validate,  authController.loginUser); // ok
 
 router.route("/refresh-token").post(authController.refreshToken);
 
