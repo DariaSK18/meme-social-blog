d30ec3f7d2219b2f856dca6ffefc302256d5e2cb
Daria Steblovska <darias1896.96@gmail.com>
2025-11-27 06:07:23 +0000

    back: get all posts check

--- server/controllers/postController.mjs
+++ server/controllers/postController.mjs
@@ -1,7 +1,7 @@
 import { catchAsync } from "../utils/catchAsync.mjs";
 import AppError from "../utils/AppError.mjs";
-import { Meme } from "../models/meme.mjs";
-import { Tag } from "../models/tag.mjs";
+import Meme from "../models/meme.mjs";
+import Tag from "../models/tag.mjs";
 
 // --- get all blogs ---
 export const getAllBlogs = catchAsync(async (req, res, next) => {
