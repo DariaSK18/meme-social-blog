365bcbf0304214c1775d4ca60a8419f4ac398458
AnnnaPiotrowska <piotrowskaannamilena@gmail.com>
2025-11-30 19:08:09 +0000

    feats: add app.jsx and removed index.css as replaced by appglobal css in styles folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,60 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import UserProfile from './components/UserProfile';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { SessionProvider } from "./context/SessionContext";
 
-import { SessionProvider } from './contexts/SessionContext';
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import UserPublicProfilePage from "./pages/UserPublicProfilePage";
+import SearchPage from "./pages/SearchPage";
 
-const App = () => {
+import HomePage from "./pages/HomePage";
+import CategoriesPage from "./pages/CategoriesPage";
+
+
+// Styling global 
+import "./styles/AppGlobals.css";
+import "./styles/Typography.css";
+import "./styles/Utilities.css";
+import "./styles/Layout.css";
+
+
+// Private Route
+import PrivateRoute from "./mock/PrivateRoute";
+
+export default function App() {
   return (
-    <div>
       <SessionProvider>
-      <Header />
-      <Routes>
-        <Route path="/" element={<CourseList />} />
-        <Route path="/profile" element={<UserProfile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<h1>Not Found</h1>} />
-      </Routes>
-      </SessionProvider>
-    </div>
-  );
-};
+        <Router>
+          <Header />
 
-export default App;
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="/profile/:id" element={<UserPublicProfilePage />} />
+            
+            <Route path="/categories" element={<CategoriesPage />} />
+            <Route path="/search" element={<SearchPage />} />
+
+
+            {/* Private Routes */}
+            <Route
+              path="/profile/me"
+              element={
+                <PrivateRoute>
+                  <UserProfilePage />
+                </PrivateRoute>
+              }
+            />
+
+            {/* Fallback */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Router>
+      </SessionProvider>
+  );
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import "./styles/AppGlobals.css";
+import App from './App.jsx'
 
-import './style.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </BrowserRouter>
-);
+  </StrictMode>,
+)
