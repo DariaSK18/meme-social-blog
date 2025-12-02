65d609d58ec5a67fff8f2c475ce0ce083be02575
Daria Steblovska <darias1896.96@gmail.com>
2025-12-02 09:18:05 +0000

    front: main layout updated

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://0.0.0.0:3000";
+const BASE_URL = "http://localhost:3000";
 
 // --- login ---
 export async function login(email, password) {
--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -2,14 +2,13 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-export default function MainLayout({children}) {
+export default function MainLayout() {
     return (
         <>
             <Header/>
                 <main className="main">
                     <Outlet/>
                 </main>
-                <main className="main">{children}</main>
             <Footer/>
         </>
     )
