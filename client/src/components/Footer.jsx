d15414c312841e9ca8208e021ee5c24fd19cabc7
Daria Steblovska <darias1896.96@gmail.com>
2025-12-02 08:29:57 +0000

    front: header structure created

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <>
+      <h1>Footer</h1>
+      <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> | <Link to="/categories">Categories</Link>
+    </>
+  );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,26 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export default function Header() {
+  const { user, logout } = useAuth();
+
   return (
-    <>
-      <h1>Header</h1>
-      <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> | <Link to="/categories">Categories</Link> |
-    </>
+    <header className="header">
+      <Link to="/">MemeSocial</Link>
+      <input type="text" placeholder="Search" />
+
+      {!user ? (
+        <div>
+          <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
+        </div>
+      ) : (
+        <div>
+          <Link to="/profile">profile</Link>
+          <button onClick={logout}>
+            Logout
+          </button>
+        </div>
+      )}
+    </header>
   );
 }
