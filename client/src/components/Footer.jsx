87082d54c920373d7bd5fcf15c75ce56c4541970
Daria Steblovska <darias1896.96@gmail.com>
2025-12-02 09:42:26 +0000

    front: footer modified

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,9 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <>
-      <h1>Footer</h1>
+    <footer className="footer">
       <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> | <Link to="/categories">Categories</Link>
-    </>
+    </footer>
   );
 }
