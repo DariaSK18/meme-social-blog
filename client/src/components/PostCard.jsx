56f4efe320af860a1913178186a13cfadbb782ce
Burcu Gunes <burcudagli.10@gmail.com>
2025-12-03 10:59:27 +0000

    post card updated

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -7,9 +7,10 @@ import {
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import Button from "../components/Button";
-import PostImage from "../assets/default-post.png";
 import { useAuth } from "../context/AuthContext";
 import { toggleLike } from "../api/postApi";
+
+const DEFAULT_POST_IMAGE = "https://i.imgflip.com/1bhk.jpg";
 
 export default function PostCard({ post }) {
   const [likesCount, setLikesCount] = useState(post.likesCount);
@@ -29,25 +30,28 @@ export default function PostCard({ post }) {
 
   return (
     <div className="post">
-      <div className="post__user user">
+      <div className="post-user user">
         {/* <img
           src={post.userAvatar}
           alt={post.username}
-          className="user__img"
+          className="user-img"
         /> */}
         <FontAwesomeIcon icon={faUser} />
-        <span className="user__username">{post.user.username}</span>
+        <span className="user-username">{post.user.username}</span>
       </div>
 
-      <div className="post__image">
+      <div className="post-image">
         <img
-          style={{ width: "25rem" }}
-          src={post.image_url.trim() !== "" ? post.image_url : PostImage}
+          src={
+            post.image_url && post.image_url.trim() !== ""
+              ? post.image_url
+              : DEFAULT_POST_IMAGE
+          }
           alt="post"
         />
       </div>
 
-      <div className="post__actions">
+      <div className="post-actions">
         <Button
           text={<FontAwesomeIcon icon={faHeart} />}
           onClick={handleToggleLike}
@@ -57,14 +61,12 @@ export default function PostCard({ post }) {
         <span>{post.commentsCount}</span>
         <Button text={<FontAwesomeIcon icon={faShare} />}></Button>
       </div>
-      <div className="post__info">
-        <span style={{ color: "green" }} className="post__description">
-          {post.user.username}
-        </span>
-        <span style={{ color: "green" }}>{post.title}</span>
-        <span>{post.description}</span>
+      <div className="post-info">
+        <span className="post-username">{post.user.username}</span>
+        {post.title && <span className="post-title">{post.title}</span>}
+        <span className="post-description">{post.description}</span>
       </div>
-      <div className="post__tags">
+      <div className="post-tags">
         {post.tags?.map((tag) => (
           <span key={tag.id}>#{tag.tag_name} </span>
         ))}
