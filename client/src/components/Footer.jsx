c5996a4099630824efff9d2c60d2bc3f6a58d26f
Burcu Gunes <burcudagli.10@gmail.com>
2025-12-03 00:12:44 +0000

    footer css added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import { Link } from "react-router-dom";
+import "../styles/footer.css";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> | <Link to="/categories">Categories</Link>
+      <div className="footer-content">
+        <div className="footer-links">
+          <Link to="/">Home</Link>
+          <Link to="/categories">Categories</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </div>
+        <div className="footer-copyright">
+          © 2025 MemeSocial. All rights reserved.
+        </div>
+      </div>
     </footer>
   );
 }
